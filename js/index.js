52c9a4abe6cb82e31e563ece8dcf1210b2a4d653
Sebastian Gylstorff <seba124m@edu.zealand.dk>
2022-11-07 13:55:41 +0100

    update til vue template

--- js/index.js
+++ js/index.js
@@ -1,2 +1,36 @@
 //Write your Javascript code here
-console.log("Shoppinglist")
+
+document.getElementById("addButton").addEventListener("click", addfunction)
+let elementInput = document.getElementById("newGoods");
+counter = 0
+
+function addfunction(){
+    let inputText = elementInput.value;
+
+    let list = document.getElementById("list");
+    let newLi = document.createElement("li");
+    let newText = document.createTextNode(inputText);
+    
+    let checkB = document.getElementById("checkBox")
+    newLi.appendChild(newText);
+    if(checkB.checked){
+        newLi.setAttribute("class","healthy");
+    }
+    else{
+        newLi.setAttribute("class","unhealthy");
+    }
+
+    
+    newLi.setAttribute("id", counter)
+    counter = counter + 1
+
+    console.log(newLi)
+    list.appendChild(newLi);
+}
+
+function loop(item,){
+    let id = item.getAttribute("id")
+    if (id >= counter){
+        counter = counter + 1
+    }
+}
